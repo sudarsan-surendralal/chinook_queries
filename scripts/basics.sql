1e119916cac606970cf63ad136058d91bddbee0c
Sudarsan Surendralal <sudarsan.sl@gmail.com>
2021-10-03 23:15:41 +0200

    Delete basics.sql

--- scripts/basics.sql
+++
@@ -1,8 +0,0 @@
-/* Set commands to print outputs */
-.header ON
-.mode column
-
--- Query all songs by Freddie Mercury or Brian May :)
-SELECT * FROM Track
-WHERE Composer LIKE "%Mercury%"
-      OR Composer LIKE "%May%";
