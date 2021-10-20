9aeec3f2fc4f9a223a8c3b6adee2c57347f29fcf
sudarsan1989 <surendralal@mpie.de>
2021-10-20 16:03:30 +0200

    Update Queen realated queries

--- scripts/queen.sql
+++ scripts/queen.sql
@@ -1,9 +1,13 @@
+/*
+This script has some basic sql queries to find more info about the band  Queen!
+*/
+
 /* Set commands to print outputs */
 .header ON
 .mode column
 
 /* Query all songs by the members of the Band :) */
-SELECT * FROM Track
+SELECT t.Name, t.Composer, t.Milliseconds FROM Track t
 WHERE (Composer LIKE "%Mercury%")
       OR (Composer LIKE "%May%" and Composer LIKE "%Brian%")
       OR (Composer LIKE "%Taylor%" and Composer LIKE "%Roger%")
